632b74778898aa58dec66bcc3f6efaf349d0fe30
ayinamubarakk <ayina.mubarak2301051@gmail.com>
2025-11-13 20:09:47 +0500

    Add files via upload

---
+++ practcequstions.py
@@ -0,0 +1,20 @@
+# # Write a program to input side of a square and its area
+
+# side = float(input("Enter the length of side:"))
+# area = side*side
+# print(area)
+
+# #write a program to input two floating points to calculate average 
+
+# a = float(input("Enter the first number :"))
+# b = float(input("Enter the second  number :"))
+
+
+# print("average =", (a+b)/2)
+
+#write a program to input 2 int number
+a = input("Enter the first number :")
+b = input("Enter the first number :")
+
+print(a >= b)
+
